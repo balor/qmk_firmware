56d833233907a0652d9193ce24c3f53e2c623e5e
balor <balor@goto.hell>
2022-12-30 23:03:33 +0100

    enhancement(balor_san): add HOLD_ON_OTHER_KEY_PRESS

--- keyboards/lily58/keymaps/balor_san/config.h
+++ keyboards/lily58/keymaps/balor_san/config.h
@@ -26,5 +26,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#ifdef TAPPING_TERM
+#undef TAPPING_TERM
+#endif // TAPPING_TERM
 #define TAPPING_FORCE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
 #define TAPPING_TERM 200
