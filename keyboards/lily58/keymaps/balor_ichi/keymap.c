4395601e430aa7c43eb3103b9a6b980671418405
balor <me@balor.pl>
2023-05-16 18:24:25 +0200

    balor_ichi + balor_san : change lay to my corne one

--- keyboards/lily58/keymaps/balor_ichi/keymap.c
+++ keyboards/lily58/keymaps/balor_ichi/keymap.c
@@ -1,10 +1,10 @@
 #include QMK_KEYBOARD_H
 enum layer_number {
-  _COLEMAK = 0,
+  _QWERTY = 0,
   _LOWER,
   _RAISE,
   _ADJUST,
-  _QWERTY,
+  _COLEMAK,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -47,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT(
-  _______,          KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     _______,
-  _______,          KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     _______,
-  _______,          KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  _______,
-  _______,          KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  _______,
-                                  _______, _______, _______, _______, _______,  _______, _______, _______
+  KC_GRV,          KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
+  KC_TAB,          KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
+  LCTL_T(KC_ESC),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+  KC_LSFT,         KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_RALT, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -59,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|   |  |   -  |   +  |   {  |   }  |   ~  |
+ * |      |      |      |      |      |      |-------.    ,-------|   \  |   -  |   =  |   [  |   ]  |   `  |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|   \  |   _  |   =  |   {  |   }  |   `  |
+ * |      |      |      |      |      |      |-------|    |-------|   |  |   _  |   +  |   {  |   }  |   ~  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE | RALT | RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -70,8 +70,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  _______, _______, _______, _______, _______, _______,                   KC_PIPE, KC_MINS, KC_PLUS, KC_LCBR,  KC_RCBR,  KC_TILD,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS, KC_UNDS, KC_EQL,  KC_LBRC,  KC_RBRC,  KC_GRV,
+  _______, _______, _______, _______, _______, _______,                   KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TILD,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
 /* RAISE
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | CAPS |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |CTRL(F1)| F2 |  F3  |  F4  |  F5  |  F6  |-------.    ,-------| Left | Down |  Up  |Right |      |Delete|
+ * |      |      |      |      |      |      |-------.    ,-------| Left | Down |  Up  |Right |      |Delete|
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------| Home |PgDown| PgUp | End  |      |Insert|
+ * |      |      |      |      |      |      |-------|    |-------| Home |PgDown| PgUp | End  |      |Insert|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE | RALT | RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -91,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  LALT_T(KC_CAPS), KC_1, KC_2, KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  LCTL_T(KC_F1),   KC_F2, KC_F3, KC_F4, KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_DEL,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, KC_INS,
+  KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+  _______, _______, _______, _______, _______, _______,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_DEL,
+  _______, _______, _______, _______, _______, _______,   _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, KC_INS,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
 /* ADJUST
@@ -305,7 +305,7 @@ static void render_anim(void) {
         /* if(timer_elapsed(anim_sleep) > SLEEP_TIMER) {
            oled_off();
         } */
-        // else 
+        // else
         if(timer_elapsed(anim_timer) > ANIM_FRAME_DURATION) {
             anim_timer = timer_read();
             animation_phase();
